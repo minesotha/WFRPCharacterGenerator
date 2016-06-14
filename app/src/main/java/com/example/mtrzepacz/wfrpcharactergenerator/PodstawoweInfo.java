082fa466e0bcddb611bac0b6b757e1fd319a483d
minesotha <minesotha@gmail.com>
2016-06-14 13:46:08 +0200

    dodanie obslugi swipa #1
    #1 podstaowwy mechanizm - wyswietla tylko okno z info o akcji

--- app/src/main/java/com/example/mtrzepacz/wfrpcharactergenerator/PodstawoweInfo.java
+++ app/src/main/java/com/example/mtrzepacz/wfrpcharactergenerator/PodstawoweInfo.java
@@ -3,87 +3,50 @@ package com.example.mtrzepacz.wfrpcharactergenerator;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.GestureDetector;
 import android.view.GestureDetector.SimpleOnGestureListener;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
+import android.widget.ScrollView;
+import android.widget.Toast;
 
 public class PodstawoweInfo extends AppCompatActivity {
-
-    @Override
+    private float x1, x2;
+    ScrollView sw;
+     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_karta);
+        sw = (ScrollView)findViewById(R.id.pi_scrollview);
+         sw.setOnTouchListener(
+                 new View.OnTouchListener() {
+                     @Override
+                     public boolean onTouch(View v, MotionEvent event) {
+                         // TODO Auto-generated method stub
+                         switch (event.getAction()) {
+                             case MotionEvent.ACTION_DOWN:
+                                 x1 = event.getX();
+                                 break;
+                             case MotionEvent.ACTION_UP:
+                                 x2 = event.getX();
+                                 float deltaX = x2 - x1;
+                                 if (deltaX < 0) {
+                                     Toast.makeText(PodstawoweInfo.this,
+                                             "Right to Left swipe",
+                                             Toast.LENGTH_SHORT).show();
+                                 }else if(deltaX >0){
+                                     Toast.makeText(PodstawoweInfo.this,
+                                             "Left to Right swipe",
+                                             Toast.LENGTH_SHORT).show();
+                                 }
+                                 break;
+                         }
+
+                         return false;
+                     }
+                 });
     }
 
-
-    public class OnSwipeTouchListener implements OnTouchListener {
-
-        private final GestureDetector gestureDetector;
-
-        public OnSwipeTouchListener (Context ctx){
-            gestureDetector = new GestureDetector(ctx, new GestureListener());
-        }
-
-        @Override
-        public boolean onTouch(View v, MotionEvent event) {
-            return gestureDetector.onTouchEvent(event);
-        }
-
-        private final class GestureListener extends SimpleOnGestureListener {
-
-            private static final int SWIPE_THRESHOLD = 100;
-            private static final int SWIPE_VELOCITY_THRESHOLD = 100;
-
-            @Override
-            public boolean onDown(MotionEvent e) {
-                return true;
-            }
-
-            @Override
-            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-                boolean result = false;
-                try {
-                    float diffY = e2.getY() - e1.getY();
-                    float diffX = e2.getX() - e1.getX();
-                    if (Math.abs(diffX) > Math.abs(diffY)) {
-                        if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
-                            if (diffX > 0) {
-                                onSwipeRight();
-                            } else {
-                                onSwipeLeft();
-                            }
-                        }
-                        result = true;
-                    }
-                    else if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
-                        if (diffY > 0) {
-                            onSwipeBottom();
-                        } else {
-                            onSwipeTop();
-                        }
-                    }
-                    result = true;
-
-                } catch (Exception exception) {
-                    exception.printStackTrace();
-                }
-                return result;
-            }
-        }
-
-        public void onSwipeRight() {
-
-        }
-
-        public void onSwipeLeft() {
-        }
-
-        public void onSwipeTop() {
-        }
-
-        public void onSwipeBottom() {
-        }
-    }
 }
