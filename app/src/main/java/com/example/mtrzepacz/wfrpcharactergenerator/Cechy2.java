e796e3e2bd84317c30d56c3e0278d9bc8f5de18c
minesotha <minesotha@gmail.com>
2016-06-15 15:59:13 +0200

    dodanie ekranu Cechy2

---
+++ app/src/main/java/com/example/mtrzepacz/wfrpcharactergenerator/Cechy2.java
@@ -0,0 +1,13 @@
+package com.example.mtrzepacz.wfrpcharactergenerator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Cechy2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cechy2);
+    }
+}
