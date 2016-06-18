78a51667d3043c8139d7237ac6cd8a0e64108fe5
minesotha <minesotha@gmail.com>
2016-06-18 23:33:06 +0200

    zapis
    działająca mechanika zapisu pól z pierwszego ekranu - podstawowe
    informacje

--- app/src/main/java/com/example/mtrzepacz/wfrpcharactergenerator/MainMenu.java
+++ app/src/main/java/com/example/mtrzepacz/wfrpcharactergenerator/MainMenu.java
@@ -7,6 +7,7 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
@@ -36,17 +37,20 @@ public class MainMenu extends AppCompatActivity {
     }
 
     public void newChara_click(View v) {
-        startActivity(new Intent(MainMenu.this, PodstawoweInfo.class));
+        Intent i = new Intent(MainMenu.this, PodstawoweInfo.class);
+        i.putExtra("startNew",true);
+        startActivity(i);
     }
 
 
     public void saveCahra_click(View v) {
         Serializer serializer = new Persister();
-        Profesja prof = new Profesja(Postac.getInstance().imie_naziwsko, 666);
+        //Profesja prof = new Profesja(Postac.getInstance().imie_naziwsko, 666);
         File file = new File(getFilesDir(), "postac.xml");
 
         try {
-            serializer.write(prof, file);
+            serializer.write(Postac.getInstance(), file);
+            Toast.makeText(getBaseContext(), "Zapisano pomyślnie postać: "+Postac.getInstance().imie_naziwsko.toString(), Toast.LENGTH_LONG).show();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -56,11 +60,15 @@ public class MainMenu extends AppCompatActivity {
         Serializer serializer = new Persister();
         File source = new File(getFilesDir(), "postac.xml");
         try {
-            Profesja prof = serializer.read(Profesja.class, source);
-            Postac.getInstance().imie_naziwsko = prof.getNazwa();
+           // Profesja prof = serializer.read(Profesja.class, source);
+            serializer.read(Postac.class, source);
+            //Postac.getInstance().imie_naziwsko = prof.getNazwa();
+            Toast.makeText(getBaseContext(), "Wczytano pomyślnie postać: "+Postac.getInstance().imie_naziwsko.toString(), Toast.LENGTH_LONG).show();
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        startActivity(new Intent(MainMenu.this, PodstawoweInfo.class));
 
     }
 
--- app/src/main/java/com/example/mtrzepacz/wfrpcharactergenerator/Postac.java
+++ app/src/main/java/com/example/mtrzepacz/wfrpcharactergenerator/Postac.java
@@ -1,15 +1,38 @@
 package com.example.mtrzepacz.wfrpcharactergenerator;
 
+import org.simpleframework.xml.Element;
+import org.simpleframework.xml.Root;
+
 import java.util.List;
 
 /**
  * Created by mtrzepacz on 13.06.2016.
  */
+@Root
 public final class Postac {
 
     //konstruktor
     private static Postac instance = null;
     protected Postac(){
+        walkaWrecz = new CechaGlowna(NazwyCech.walkaWrecz,0,0);
+        umiejetnosciStrzeleckie = new CechaGlowna(NazwyCech.umiejetnosciStrzeleckie,0,0);
+        krzepa = new CechaGlowna(NazwyCech.krzepa,0,0);
+        odpornosc = new CechaGlowna(NazwyCech.odpornosc,0,0);
+        zrecznosc = new CechaGlowna(NazwyCech.zrecznosc,0,0);
+        inteligencja = new CechaGlowna(NazwyCech.inteligencja,0,0);
+        silaWoli = new CechaGlowna(NazwyCech.silaWoli,0,0);
+        oglada = new CechaGlowna(NazwyCech.oglada,0,0);
+
+        ataki = new CechaDrugorzedna(NazwyCech.ataki,0,0);
+        zywotnosc = new CechaDrugorzedna(NazwyCech.zywotnosc,0,0);
+        sila = new CechaDrugorzedna(NazwyCech.sila,0,0);
+        wytrzymalosc= new CechaDrugorzedna(NazwyCech.wytrzymalosc,0,0);
+        szybkosc= new CechaDrugorzedna(NazwyCech.szybkosc,0,0);
+        magia = new CechaDrugorzedna(NazwyCech.magia,0,0);
+        punktyObledu = new CechaDrugorzedna(NazwyCech.punktyObledu,0,0);
+        punktyPrzeznaczenia= new CechaDrugorzedna(NazwyCech.punktyPrzeznaczenia,0,0);
+
+
         //TODO - losowane rzeczy?
     }
     public static Postac getInstance(){
@@ -20,59 +43,92 @@ public final class Postac {
     }
 
     //podstawowe info
-    public String imie_naziwsko;
-    public String rasa;
-    public String profesja;
-    public int expAll;
-    public int expCurrent;
+    @Element
+    public String imie_naziwsko="";
+    @Element
+    public String rasa="";
+    @Element
+    public String profesja="";
+    @Element
+    public String poprzedniaprofesja="";
+    @Element
+    public int expAll =0;
+    @Element
+    public int expCurrent=0;
 
     //dodatekowe info
-    public int wiek;
-    public String kolor_wlosow;
-    public String kolor_oczu;
-    public Boolean czyMężczyzna;
-    public int waga;
-    public  int wzrost;
-    public  String rodzeństwo;
-    public  String miejsce_urodzenia;
-    public  String znaki_szczegolne;
-    public  String znak_gwiezdny;
+    @Element
+    public int wiek=0;
+    @Element
+    public String kolor_wlosow="";
+    @Element
+    public String kolor_oczu="";
+    @Element
+    public Boolean czyMężczyzna=false;
+    @Element
+    public int waga=0;
+    @Element
+    public  int wzrost=0;
+    @Element
+    public  String rodzeństwo="";
+    @Element
+    public  String miejsce_urodzenia="";
+    @Element
+    public  String znaki_szczegolne="";
+    @Element
+    public  String znak_gwiezdny="";
 
     //Cechy główne
     /**walka wręcz*/
+    @Element
     public  CechaGlowna walkaWrecz;
     /** umiejętności strzeleckie*/
+    @Element
     public  CechaGlowna umiejetnosciStrzeleckie;
     /** krzepa */
+    @Element
     public  CechaGlowna krzepa;
     /** odporność */
+    @Element
     public   CechaGlowna odpornosc;
     /** zręcznosć */
+    @Element
     public    CechaGlowna zrecznosc;
     /** inteligencja */
+    @Element
     public   CechaGlowna inteligencja;
     /** siła woli */
+    @Element
     public   CechaGlowna silaWoli;
     /** ogłada */
+    @Element
     public   CechaGlowna oglada;
 
 
     //cechy drugorzędne
     /** ataki */
+    @Element
     public CechaDrugorzedna ataki;
     /** żywotność */
+    @Element
     public CechaDrugorzedna zywotnosc;
     /** siła */
+    @Element
     public CechaDrugorzedna sila;
     /** wytrzymalosc */
+    @Element
     public CechaDrugorzedna wytrzymalosc;
     /**szybkosc */
+    @Element
     public CechaDrugorzedna szybkosc;
     /**magia */
+    @Element
     public CechaDrugorzedna magia;
     /** punkty obledu */
+    @Element
     public CechaDrugorzedna punktyObledu;
     /** punkty przeznaczenia */
+    @Element
     public CechaDrugorzedna punktyPrzeznaczenia;
 
 
