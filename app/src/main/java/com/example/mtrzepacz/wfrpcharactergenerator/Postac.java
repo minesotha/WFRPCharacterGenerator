a6469d2f1b3a40508e20589dd9ef69012992fe60
minesotha <minesotha@gmail.com>
2016-06-20 13:22:34 +0200

    ustawienie zapisu

--- app/src/main/java/com/example/mtrzepacz/wfrpcharactergenerator/Postac.java
+++ app/src/main/java/com/example/mtrzepacz/wfrpcharactergenerator/Postac.java
@@ -14,23 +14,7 @@ public final class Postac {
     //konstruktor
     private static Postac instance = null;
     protected Postac(){
-        walkaWrecz = new CechaGlowna(NazwyCech.walkaWrecz,0,0);
-        umiejetnosciStrzeleckie = new CechaGlowna(NazwyCech.umiejetnosciStrzeleckie,0,0);
-        krzepa = new CechaGlowna(NazwyCech.krzepa,0,0);
-        odpornosc = new CechaGlowna(NazwyCech.odpornosc,0,0);
-        zrecznosc = new CechaGlowna(NazwyCech.zrecznosc,0,0);
-        inteligencja = new CechaGlowna(NazwyCech.inteligencja,0,0);
-        silaWoli = new CechaGlowna(NazwyCech.silaWoli,0,0);
-        oglada = new CechaGlowna(NazwyCech.oglada,0,0);
 
-        ataki = new CechaDrugorzedna(NazwyCech.ataki,0,0);
-        zywotnosc = new CechaDrugorzedna(NazwyCech.zywotnosc,0,0);
-        sila = new CechaDrugorzedna(NazwyCech.sila,0,0);
-        wytrzymalosc= new CechaDrugorzedna(NazwyCech.wytrzymalosc,0,0);
-        szybkosc= new CechaDrugorzedna(NazwyCech.szybkosc,0,0);
-        magia = new CechaDrugorzedna(NazwyCech.magia,0,0);
-        punktyObledu = new CechaDrugorzedna(NazwyCech.punktyObledu,0,0);
-        punktyPrzeznaczenia= new CechaDrugorzedna(NazwyCech.punktyPrzeznaczenia,0,0);
 
 
         //TODO - losowane rzeczy?
@@ -77,6 +61,8 @@ public final class Postac {
         Postac.getInstance().magia = new CechaDrugorzedna(NazwyCech.magia,0,0);
         Postac.getInstance().punktyObledu = new CechaDrugorzedna(NazwyCech.punktyObledu,0,0);
         Postac.getInstance().punktyPrzeznaczenia= new CechaDrugorzedna(NazwyCech.punktyPrzeznaczenia,0,0);
+
+
     }
 
     //podstawowe info
@@ -117,55 +103,55 @@ public final class Postac {
 
     //Cechy główne
     /**walka wręcz*/
-    @Element
+   // @Element
     public  CechaGlowna walkaWrecz;
     /** umiejętności strzeleckie*/
-    @Element
+   // @Element
     public  CechaGlowna umiejetnosciStrzeleckie;
     /** krzepa */
-    @Element
+    //@Element
     public  CechaGlowna krzepa;
     /** odporność */
-    @Element
+  //  @Element
     public   CechaGlowna odpornosc;
     /** zręcznosć */
-    @Element
+   // @Element
     public    CechaGlowna zrecznosc;
     /** inteligencja */
-    @Element
+  //  @Element
     public   CechaGlowna inteligencja;
     /** siła woli */
-    @Element
+  //  @Element
     public   CechaGlowna silaWoli;
     /** ogłada */
-    @Element
+  //  @Element
     public   CechaGlowna oglada;
 
 
     //cechy drugorzędne
     /** ataki */
-    @Element
+   // @Element
     public CechaDrugorzedna ataki;
     /** żywotność */
-    @Element
+  //  @Element
     public CechaDrugorzedna zywotnosc;
     /** siła */
-    @Element
+   // @Element
     public CechaDrugorzedna sila;
     /** wytrzymalosc */
-    @Element
+   // @Element
     public CechaDrugorzedna wytrzymalosc;
     /**szybkosc */
-    @Element
+   // @Element
     public CechaDrugorzedna szybkosc;
     /**magia */
-    @Element
+  //  @Element
     public CechaDrugorzedna magia;
     /** punkty obledu */
-    @Element
+   // @Element
     public CechaDrugorzedna punktyObledu;
     /** punkty przeznaczenia */
-    @Element
+   // @Element
     public CechaDrugorzedna punktyPrzeznaczenia;
 
 
